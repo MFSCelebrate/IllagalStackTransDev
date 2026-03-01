c7b6dab007b282ed368db52f7fddcaad962a49c4
MFSCelebrate <19358185571@163.com>
2026-03-02 00:08:47 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,10 @@ repositories {
         name = "PaperMC"
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
-    maven {
-        name = "ProtocolLib"
-        url = uri("https://repo.dmulloy2.net/nexus/repository/public/")
-    }
+maven {
+           url "https://repo.dmulloy2.net/repository/public/"
+       }
+    
     maven {
         name = "Magic"
         url = uri("https://maven.elmakers.com/repository/")
@@ -40,7 +40,7 @@ repositories {
 
 dependencies {
     compileOnly("dev.folia:folia-api:1.20.4-R0.1-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:4.8.0")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
     compileOnly("com.elmakers.mine.bukkit:MagicAPI:10.2")
     compileOnly("de.tr7zw:item-nbt-api-plugin:2.8.0")
     compileOnly("com.github.TheBusyBiscuit:Slimefun4:RC-30") { isTransitive = false }
