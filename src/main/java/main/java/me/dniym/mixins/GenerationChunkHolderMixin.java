06d7f15ce2d3deb55588bb8d6439d80367e1d0b2
MFSCelebrate <19358185571@163.com>
2026-03-15 11:09:08 +0800

    Update GenerationChunkHolderMixin.java

--- src/main/java/main/java/me/dniym/mixins/GenerationChunkHolderMixin.java
+++ src/main/java/main/java/me/dniym/mixins/GenerationChunkHolderMixin.java
@@ -2,10 +2,10 @@ package main.java.me.dniym.mixins;
 
 import net.minecraft.server.level.GenerationChunkHolder;
 import net.minecraft.world.level.ChunkPos;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import main.java.me.dniym.mixin.asm.mixin.Mixin;
+import main.java.me.dniym.mixin.asm.mixin.injection.At;
+import main.java.me.dniym.mixin.asm.mixin.injection.Inject;
+import main.java.me.dniym.mixin.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * 使 GenerationChunkHolder 完全失效：任何实例化都会抛出异常。
