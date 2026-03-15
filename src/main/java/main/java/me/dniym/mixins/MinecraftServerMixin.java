44dbc87e1276b41b7ba1f2d4ae22b1ff5c4fff9a
MFSCelebrate <19358185571@163.com>
2026-03-15 10:15:12 +0800

    Update MinecraftServerMixin.java

--- src/main/java/main/java/me/dniym/mixins/MinecraftServerMixin.java
+++ src/main/java/main/java/me/dniym/mixins/MinecraftServerMixin.java
@@ -2,21 +2,22 @@ package main.java.me.dniym.mixins;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 /**
- * Mixin 用于修改 MinecraftServer 中世界边界的绝对最大尺寸。
- * 将默认的 30,000,000 (0x01c9c370) 改为 1,073,741,824。
+ * Mixin 用于修改 MinecraftServer 中世界边界的绝对最大尺寸常量。
  */
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
 
     /**
-     * @author 你的名字
-     * @reason 覆盖原方法，返回自定义的绝对最大世界尺寸。
+     * 修改 ABSOLUTE_MAX_WORLD_SIZE 常量的值。
+     * 原值：0x01c9c370 (30,000,000)
+     * 新值：1073741824 (2^30)
      */
-    @Overwrite
-    public int getAbsoluteMaxWorldSize() {
-        return 1073741824; // 2^30
+    @ModifyConstant(method = "<clinit>", constant = @Constant(intValue = 0x01c9c370))
+    private int modifyAbsoluteMaxWorldSize(int original) {
+        return 1073741824;
     }
 }
