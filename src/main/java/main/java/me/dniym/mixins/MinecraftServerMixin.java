5c37394a5b02a3ca30087ead24890c7839369894
MFSCelebrate <19358185571@163.com>
2026-03-21 22:24:21 +0800

    Update MinecraftServerMixin.java

--- src/main/java/main/java/me/dniym/mixins/MinecraftServerMixin.java
+++ src/main/java/main/java/me/dniym/mixins/MinecraftServerMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
-    @ModifyConstant(method = "*", constant = @Constant(intValue = 30000000))
+    @ModifyConstant(method = "*", constant = @Constant(intValue = 30000000), remap = false)
     private int modifyMaxWorldSize(int original) {
         System.out.println("[IllegalStack] Mixin applied! Modifying max world size from " + original + " to 2147483646");
         return 2147483646;
