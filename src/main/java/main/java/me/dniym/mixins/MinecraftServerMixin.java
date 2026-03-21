edae5bc4a38e1efa69781696aa51713f462643b8
MFSCelebrate <19358185571@163.com>
2026-03-21 17:44:56 +0800

    Create MinecraftServerMixin.java

---
+++ src/main/java/main/java/me/dniym/mixins/MinecraftServerMixin.java
@@ -0,0 +1,14 @@
+package main.java.me.dniym.mixins;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @ModifyConstant(method = "getAbsoluteMaxWorldSize", constant = @Constant(intValue = 30000000))
+    private int modifyMaxWorldSize(int original) {
+        return 2147483646; // Integer.MAX_VALUE - 1
+    }
+}
