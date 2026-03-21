884995b0475ba8d7091256363c0565a8675d570a
MFSCelebrate <19358185571@163.com>
2026-03-21 17:45:41 +0800

    Create WorldBorderCommandMixin.java

---
+++ src/main/java/main/java/me/dniym/mixins/WorldBorderCommandMixin.java
@@ -0,0 +1,19 @@
+package main.java.me.dniym.mixins;
+
+import net.minecraft.commands.arguments.WorldBorderCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(WorldBorderCommand.class)
+public class WorldBorderCommandMixin {
+    @ModifyConstant(method = "*", constant = @Constant(doubleValue = 5.9999968E7))
+    private double modifyMaxSize(double original) {
+        return 2147483646.0;
+    }
+
+    @ModifyConstant(method = "*", constant = @Constant(doubleValue = 2.9999984E7))
+    private double modifyHalfMaxSize(double original) {
+        return 1073741823.0;
+    }
+}
